a84670d1d422b0ecb4835a5972a552a40586af7f
webAnkit23 <ankitsingh2323@gmail.com>
2024-02-26 17:51:41 +0530

    first commit

--- src/Components/homeSection/HomeSection.jsx
+++ src/Components/homeSection/HomeSection.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
-import Hero from './hero'
-import Navbar from '../Navbar/Navbar'
+import Hero from './Hero.jsx'
 import MainContent from '../mainContent/MainContent'
-import useFetch from '../../Hooks/useFetch'
 export default function HomeSection() {
-
-  
   return (
     <>
-
-       <Hero />
+      <Hero />
        <MainContent />
     </>
   
